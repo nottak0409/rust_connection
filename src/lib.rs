1c480387b174e66b7756b10c06cb441ba4e1f304
nottak0409 <nottak0409@gmail.com>
2022-06-22 18:31:23 +0900

    [add]threadpoolの更新

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 use std::thread;
+use std::sync::mpsc;
+use std::sync::Mutex;
+use std::sync::Arc;
 
 pub struct ThreadPool {
-    workers: Vec<Worker>
+    workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
 
+struct Job;
 
 impl ThreadPool {
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
+        let (sender, receiver) = mpsc::channel();
+
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
+
         for id in 0..size {
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, Arc::clone(&receiver)))
         }
 
         ThreadPool {
-            workers
+            workers,
+            sender
         }
     }
 
@@ -30,13 +41,14 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(|| {
+            receiver;
         });
 
         Worker {
             id,
-            thread
+            thread,
         }
     }
 }
