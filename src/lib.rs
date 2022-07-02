7ac7d7d54503e374901f8c30f656ea56eee85062
nottak0409 <nottak0409@gmail.com>
2022-07-02 13:14:54 +0900

    [add]変更

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,11 @@ use std::sync::mpsc;
 use std::sync::Mutex;
 use std::sync::Arc;
 
+enum Message {
+    NewJob(Job),
+    Terminate,
+}
+
 trait FnBox {
     fn call_box(self: Box<Self>);
 }
@@ -15,7 +20,7 @@ impl<F: FnOnce()> FnBox for F {
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnBox + Send + 'static>;
@@ -40,13 +45,15 @@ impl ThreadPool {
         }
     }
 
+
     pub fn execute<F>(&self, f: F)
     where F: FnOnce() + Send + 'static {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
     }
 }
+
 
 pub struct Worker {
     id: usize,
@@ -54,14 +61,22 @@ pub struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Worker {
         let thread = thread::spawn(move || {
             loop {
-                let job = receiver.lock().unwrap().recv().unwrap();
+                let message = receiver.lock().unwrap().recv().unwrap();
 
-                println!("ワーカーは{}を実行します", id);
+                match message {
+                    Message::NewJob(job) => {
+                        println!("ワーカーは{}を実行します", id);
+                    }
 
-                job.call_box();
+                    Message::Terminate => {
+                        println!("ワーカーは{}停止です", id);
+
+                        break;
+                    },
+                }
             };
         });
 
@@ -74,8 +89,18 @@ impl Worker {
 
 impl Drop for ThreadPool {
     fn drop(&mut self) {
+        println!("Sending terminate message to all workers.");
+
+        for _ in &mut self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        // 全ワーカーを閉じます
+        println!("Shutting down all workers.");
+
         for worker in &mut self.workers {
-            println!("ワーカーを終了します {}", worker.id);
+            // ワーカー{}を閉じます
+            println!("Shutting down worker {}", worker.id);
 
             if let Some(thread) = worker.thread.take() {
                 thread.join().unwrap();
